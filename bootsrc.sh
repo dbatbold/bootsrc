3063711ecb43c2c1b17ba0b9c5ea48559fe1ceb6
Batbold Dashzeveg <dbatbold@gmail.com>
2020-04-21 00:51:26 -0700

    Update bootsrc.sh

--- bootsrc.sh
+++ bootsrc.sh
@@ -1,12 +1,12 @@
-# Site URL that serves source and binary files for bootsrc.
-# The site can also be used for caching Archlinux packages for faster builds.
+# Optional local package server used in caching source files
+# and Archlinux binary packages for faster access.
 export PKG_SITE=http://zero-file
 
-# User ID with write access to /bootsrc install directory.
+# Non-root build user ID.
 export USER_ID=1000
 
 # LLVM version.
 export LLVM_VER=10.0.0
 
-# RSA key that signed LLVM sources.
+# RSA key that signed LLVM source files.
 export LLVM_KEY=B6C8F98282B944E3B0D5C2530FC3042E345AD05D
